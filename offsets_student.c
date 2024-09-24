d7d7fbc2efc97ff2fc071d0c00df55412b3fe449
Heman Burre <hsb26@duke.edu>
2024-09-23 22:10:57 -0400

    finished offsets

--- offsets_student.c
+++ offsets_student.c
@@ -5,17 +5,21 @@
 #include "structs.h"
 
 size_t offsetsS0(int arrayIndex, int fieldIndex) {
-        // YOUR CODE HERE...
+        size_t offsets0[] = {0, 4, 16, 24};
+        return (arrayIndex * sizeof(struct S0) + offsets0[fieldIndex]);
 }
 
 size_t offsetsS1(int arrayIndex, int fieldIndex) {
-        // YOUR CODE HERE...
+        size_t offsets1[] = {0, 8, 16, 18};
+        return (arrayIndex * sizeof(struct S1) + offsets1[fieldIndex]);
 }
 
 size_t offsetsS2(int arrayIndex, int fieldIndex) {
-        // YOUR CODE HERE...
+        size_t offsets2[] = {0, 4, 8, 16};
+        return (arrayIndex * sizeof(struct S2) + offsets2[fieldIndex]);
 }
 
 size_t offsetsS3(int arrayIndex, int fieldIndex) {
-        // YOUR CODE HERE...
+        size_t offsets3[] = {0, 8, 12, 14};
+        return (arrayIndex * sizeof(struct S3) + offsets3[fieldIndex]);
 }
